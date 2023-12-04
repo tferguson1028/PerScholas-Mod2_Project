51698cd445469a15dcc399f739e58d84c0e8273c
Tesean Ferguson <tferguson1028@outlook.com>
2023-12-04 16:04:38 -0500

    Tested sound playing via URI

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,30 @@ function App() {
     console.log("GGG");
   }
 
+  async function playSound(url)
+  {
+    let sound = new Audio(url);
+    let sound2 = new Audio("https://cdn.freesound.org/previews/506/506545_6142149-lq.ogg");
+    let audio = new Audio('https://www2.cs.uic.edu/~i101/SoundFiles/StarWars60.wav');  
+    audio.type = 'audio/wav';
+
+    try 
+    {
+      // await sound.play();
+      await sound2.play();
+      // await audio.play();
+      console.log('Playing...');
+    } catch (err) 
+    {
+      console.log('Failed to play...' + err);
+    }
+  }
+
+  playSound("https://cdn.freesound.org/previews/506/506545_6142149-lq.ogg");
+
   return (
     <div className="App">
-      <div style={{position: "fixed", top: "100px", backgroundColor: "blue", width: "70px", height: "70px"}}>
-        <Frag_Clickable dispatch={dis}/>
-      </div>
+      <button onClick={playSound}></button>
     </div>
   );
 }
