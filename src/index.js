07864fbe2c8f2298d36babaae144de25f47a2b19
Tesean Ferguson <tferguson1028@outlook.com>
2023-12-07 21:57:40 -0500

    Removed CurrentCompanyContext as it went unused

--- src/index.js
+++ src/index.js
@@ -6,18 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import ApiContextProvider from './contexts/APIRequestContext';
 import App from './App';
 import AccountContextProvider from './contexts/AccountContext';
-import CurrentCompanyContextProvider from './contexts/CurrentCompanyContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AccountContextProvider>
     <ApiContextProvider>
-    <CurrentCompanyContextProvider>
     
         <App />
         
-    </CurrentCompanyContextProvider>
     </ApiContextProvider>
     </AccountContextProvider>
   </React.StrictMode>
