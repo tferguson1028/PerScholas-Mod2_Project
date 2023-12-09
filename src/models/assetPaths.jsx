16adc554d64b7f609dd9ce0f06ca94dfd6fbcec3
Tesean Ferguson <tferguson1028@outlook.com>
2023-12-09 18:12:21 -0500

    Created asset paths model

---
+++ src/models/assetPaths.jsx
@@ -0,0 +1,14 @@
+const icons = {
+  arrowLeft: "/assets/icons/#",
+};
+
+const images = {
+  stocks0: "/assets/images/stockImage0.jpg",
+  stocks1: "/assets/images/stockImage0.jpg",
+  stocks2: "/assets/images/stockImage0.jpg",
+  stocks3: "/assets/images/stockImage0.jpg",
+  stocks4: "/assets/images/stockImage0.jpg",
+  stocks5: "/assets/images/stockImage0.jpg",
+}
+
+module.exports = { icons, images };
