0813ed8c2032661eb0588c2d1bf298f4da5fed48
Tesean Ferguson <tferguson1028@outlook.com>
2023-12-09 14:41:11 -0500

    Minor layout and styling

--- src/models/pages.jsx
+++ src/models/pages.jsx
@@ -1,11 +1,11 @@
 const pages = {
   homePage: { textContent: "Home", path: "home"},
+  stockSearchPage: { textContent: "Stock Search", path: "stocks"},
+  summaryPage: { textContent: "Summary", path: "summary"},
   accountPage: { textContent: "Account", path: "account", sub: {
     login: { textContent: "Login", path: "login" },
     signup: { textContent: "Sign Up", path: "signup" }
-  }},
-  stockSearchPage: { textContent: "Stock Search", path: "stocks"},
-  summaryPage: { textContent: "Summary", path: "summary"}
+  }}
 };
 
 module.exports = pages;
