e0b6fddb22886c2c48d4cf0c8feff3e1f2d7d949
Tesean Ferguson <tferguson1028@outlook.com>
2023-12-11 10:09:05 -0500

    Fixed error where company data is not an object

--- src/components/routes/CompanyStockPage.jsx
+++ src/components/routes/CompanyStockPage.jsx
@@ -47,8 +47,8 @@ function CompanyStockPage()
   
   function loaded()
   {
-    const data = companyData[0]; // API returns info as array of length 1.
-    if (Array(data).length == 0)
+    const data = companyData[0]; // API returns info as array of length 1 with an object.
+    if (data == null || typeof data != "object")
       return noCompanyError();
     else
       return (
